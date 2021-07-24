e2a0e821e06bf24ab99d1fcb9288cab302030b29
winmord <winmord@yandex.ru>
2021-07-24 17:17:54 +0300

    Fixed navigation

--- app/src/main/java/com/mtsteta/homework/presentation/MainActivity.kt
+++ app/src/main/java/com/mtsteta/homework/presentation/MainActivity.kt
@@ -1,51 +1,36 @@
 package com.mtsteta.homework.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mtsteta.homework.R
-import com.mtsteta.homework.data.dto.CategoryDto
-import com.mtsteta.homework.data.dto.MovieDto
-import com.mtsteta.homework.data.features.categories.CategoriesDataSourceImpl
-import com.mtsteta.homework.data.features.movies.MoviesDataSourceImpl
-import com.mtsteta.homework.presentation.fragments.HomeFragment
-import com.mtsteta.homework.presentation.fragments.ProfileFragment
-import com.mtsteta.homework.presentation.models.CategoriesModel
-import com.mtsteta.homework.presentation.models.MoviesModel
-import com.mtsteta.homework.presentation.recyclerviews.adapters.CategoriesRecyclerAdapter
-import com.mtsteta.homework.presentation.recyclerviews.adapters.MoviesRecyclerAdapter
-import com.mtsteta.homework.presentation.recyclerviews.decorations.BottomSpaceItemDecoration
-import com.mtsteta.homework.presentation.recyclerviews.decorations.RightSpaceItemDecoration
-import com.mtsteta.homework.presentation.recyclerviews.diffutils.callbacks.CategoriesCallback
-import com.mtsteta.homework.presentation.recyclerviews.diffutils.callbacks.MoviesCallback
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
 
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.nav_view)
+        setupBottomNavigation()
+    }
 
-        bottomNavigationView.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.navigation_home -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.nav_host_fragment_activity_navigation, HomeFragment())
-                        .commit()
-                }
-                R.id.navigation_profile -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.nav_host_fragment_activity_navigation, ProfileFragment())
-                        .commit()
-                }
-            }
-            supportFragmentManager.popBackStack()
-            true
-        }
+    private fun setupBottomNavigation() {
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+            ?.findNavController()
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_profile
+            )
+        )
+        setupActionBarWithNavController(navController!!, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
