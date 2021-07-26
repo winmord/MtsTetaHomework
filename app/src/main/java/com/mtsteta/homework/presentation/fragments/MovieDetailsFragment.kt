050fd948096ee1eb8a8fb72f2d9b6b1560514a39
winmord <winmord@yandex.ru>
2021-07-26 21:03:48 +0300

    Initiation of views moved into separate class

--- app/src/main/java/com/mtsteta/homework/presentation/fragments/MovieDetailsFragment.kt
+++ app/src/main/java/com/mtsteta/homework/presentation/fragments/MovieDetailsFragment.kt
@@ -14,6 +14,15 @@ import com.mtsteta.homework.data.dto.MovieDto
 
 class MovieDetailsFragment : Fragment() {
     private var movieDto: MovieDto? = null
+    private lateinit var movieTitleTextView: TextView
+    private lateinit var movieDescriptionTextView: TextView
+    private lateinit var movieAgeRestrictionTextView: TextView
+    private lateinit var movieCoverImageView: ImageView
+    private lateinit var movieStar1ImageView: ImageView
+    private lateinit var movieStar2ImageView: ImageView
+    private lateinit var movieStar3ImageView: ImageView
+    private lateinit var movieStar4ImageView: ImageView
+    private lateinit var movieStar5ImageView: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,27 +32,35 @@ class MovieDetailsFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_movie_details, container, false)
 
         movieDto = arguments?.getParcelable(movie_dto_bundle_key)
-        setupViews(view, movieDto)
+
+        initViews(view)
+        setupViews(movieDto)
 
         return view
     }
 
-    private fun setupViews(view: View, movieDto: MovieDto?) {
-        view.findViewById<TextView>(R.id.textMovieTitle).text = movieDto?.title
-        view.findViewById<TextView>(R.id.textMovieDescription).text = movieDto?.description
-        view.findViewById<TextView>(R.id.textMovieAgeRestriction).text =
-            getString(R.string.age_restriction, movieDto?.ageRestriction.toString())
-        view.findViewById<ImageView>(R.id.ivMovieCover).load(movieDto?.imageUrl)
+    private fun initViews(view: View) {
+        movieTitleTextView = view.findViewById(R.id.textMovieTitle)
+        movieDescriptionTextView = view.findViewById(R.id.textMovieDescription)
+        movieAgeRestrictionTextView = view.findViewById(R.id.textMovieAgeRestriction)
+        movieCoverImageView = view.findViewById(R.id.ivMovieCover)
+        movieStar1ImageView = view.findViewById(R.id.ivMovieStar1)
+        movieStar2ImageView = view.findViewById(R.id.ivMovieStar2)
+        movieStar3ImageView = view.findViewById(R.id.ivMovieStar3)
+        movieStar4ImageView = view.findViewById(R.id.ivMovieStar4)
+        movieStar5ImageView = view.findViewById(R.id.ivMovieStar5)
+    }
+
+    private fun setupViews(movieDto: MovieDto?) {
         val rateScore = movieDto?.rateScore!!
-        view.findViewById<ImageView>(R.id.ivMovieStar1)
-            .setImageResource(if (rateScore > 0) R.drawable.ic_star_black else R.drawable.ic_star_transparent)
-        view.findViewById<ImageView>(R.id.ivMovieStar2)
-            .setImageResource(if (rateScore > 1) R.drawable.ic_star_black else R.drawable.ic_star_transparent)
-        view.findViewById<ImageView>(R.id.ivMovieStar3)
-            .setImageResource(if (rateScore > 2) R.drawable.ic_star_black else R.drawable.ic_star_transparent)
-        view.findViewById<ImageView>(R.id.ivMovieStar4)
-            .setImageResource(if (rateScore > 3) R.drawable.ic_star_black else R.drawable.ic_star_transparent)
-        view.findViewById<ImageView>(R.id.ivMovieStar5)
-            .setImageResource(if (rateScore > 4) R.drawable.ic_star_black else R.drawable.ic_star_transparent)
+        movieTitleTextView.text = movieDto.title
+        movieDescriptionTextView.text = movieDto.description
+        movieAgeRestrictionTextView.text = getString(R.string.age_restriction, movieDto.ageRestriction.toString())
+        movieCoverImageView.load(movieDto.imageUrl)
+        movieStar1ImageView.setImageResource(if (rateScore > 0) R.drawable.ic_star_black else R.drawable.ic_star_transparent)
+        movieStar2ImageView.setImageResource(if (rateScore > 1) R.drawable.ic_star_black else R.drawable.ic_star_transparent)
+        movieStar3ImageView.setImageResource(if (rateScore > 2) R.drawable.ic_star_black else R.drawable.ic_star_transparent)
+        movieStar4ImageView.setImageResource(if (rateScore > 3) R.drawable.ic_star_black else R.drawable.ic_star_transparent)
+        movieStar5ImageView.setImageResource(if (rateScore > 4) R.drawable.ic_star_black else R.drawable.ic_star_transparent)
     }
 }
