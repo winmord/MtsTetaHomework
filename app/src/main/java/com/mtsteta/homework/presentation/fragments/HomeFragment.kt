8d0555b2650598ff879cfbca2eec3ce43afd604c
winmord <winmord@yandex.ru>
2021-07-29 01:36:17 +0300

    Added coroutine exceptions handling.

--- app/src/main/java/com/mtsteta/homework/presentation/fragments/HomeFragment.kt
+++ app/src/main/java/com/mtsteta/homework/presentation/fragments/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.mtsteta.homework.presentation.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -26,10 +27,7 @@ import com.mtsteta.homework.presentation.recyclerviews.decorations.BottomSpaceIt
 import com.mtsteta.homework.presentation.recyclerviews.decorations.RightSpaceItemDecoration
 import com.mtsteta.homework.presentation.recyclerviews.diffutils.callbacks.CategoriesCallback
 import com.mtsteta.homework.presentation.recyclerviews.diffutils.callbacks.MoviesCallback
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 class HomeFragment : Fragment() {
     private lateinit var moviesModel: MoviesModel
@@ -42,6 +40,9 @@ class HomeFragment : Fragment() {
     private var categories = listOf<CategoryDto>()
     private var movies = listOf<MovieDto>()
     private val activityCallbackFunction: (String) -> Unit = { showToast(it) }
+    private val coroutineExceptionHandler = CoroutineExceptionHandler { _, exception ->
+        Log.d("coroutineException", "handled $exception")
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -119,7 +120,7 @@ class HomeFragment : Fragment() {
     }
 
     private fun updateData() {
-        CoroutineScope(Dispatchers.Main).launch {
+        CoroutineScope(Dispatchers.Main).launch(coroutineExceptionHandler) {
             categoriesAdapter.categories = withContext(Dispatchers.IO) {
                 categoriesModel.getCategories()
             }
